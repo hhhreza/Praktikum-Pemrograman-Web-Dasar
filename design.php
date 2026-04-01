36acf77d2fb92e792c7801987a9260b34f12e8c0
hhhreza <dwikyreza145@gmail.com>
2026-04-01 23:04:49 +0700

    add design.php

---
+++ design.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Design</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="tampilan.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-atas" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Komunitas Kucing</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="daftar.php">Daftar</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link active" href="design.php">Design</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</nav>
+
+    <div class="container informasi">
+        <h2 class="header text-center mt-5">Design Laboratory</h2>
+
+        
+
+    
+</body>
+</html>
