0be60ce1a39f10db9d1edf2a3143c1a2f805b5e3
hhhreza <dwikyreza145@gmail.com>
2026-04-01 21:52:10 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="tampilan.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-atas" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Komunitas Kucing</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="login.php">Daftar</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="design.php">Design</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container informasi">
+        <h2 class="header">Selamat Datang di Komunitas Kucing</h2>
+
+        <div class="gambar row">
+            <div class="col-md-6">
+                <img src="kucing.jpg" alt="Kucing" class="gambar-kucing">
+            </div>
+      
+
+            <div class="col-md-7 text">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, recusandae soluta incidunt animi tempora distinctio iure sit laboriosam obcaecati impedit repellat accusamus labore nam commodi corporis possimus blanditiis odit earum?</p>
+
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae neque, veniam ipsum, at soluta non dignissimos, fuga odio laudantium incidunt tempore laborum quasi et fugit facere ipsa harum cupiditate delectus.</p>
+
+                <h5>Jenis-Jenis Kucing</h5>
+                <ol>
+                    <li>Kucing Persia</li>
+                    <li>Kucing Maine Coon</li>
+                    <li>Kucing American Shorthair</li>
+                    <li>Kucing Sphynxes</li>
+                    <li>Kucing British Shorthair</li>
+                    <li>DLL.</li>
+                </ol>
+            </div>
+        </div>
+
+
+        <a class="btn btn-primary" href="https://wolipop.detik.com/home/d-5180681/15-jenis-kucing-peliharaan-yang-populer-menggemaskan-dan-mudah-dirawat" role="button" target="_blank">Informasi Lebih Lanjut</a>
+        
+    </div>
+</body>
+</html>
